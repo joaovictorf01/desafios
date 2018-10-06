beca7f8b16a71ef49471898da42de32491db2c8e
Victor Fernandes <joaovictorf01@gmail.com>
2018-10-05 23:27:02 -0300

    Adicionada questão 2!

---
+++ desafio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+
+
+namespace desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double r = double.Parse(Console.ReadLine());
+
+            const double p = 3.14159;
+       
+     
+            double a = p * Math.Pow(
+                r, 2);
+
+            Console.Write("A=" + a.ToString("0.0000"));
+
+
+
+
+
+
+
+            Console.WriteLine();
+
+
+
+
+
+
+        }
+    } }
--- desafios/Program.cs
+++ desafios/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace desafios
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
+            int A = Int32.Parse(Console.ReadLine());
+            int B = Int32.Parse(Console.ReadLine());
+            int X = A + B;
+
+            Console.Write("X = " + X);
+
+            Console.WriteLine();
+
+
+           }
     }
-}
+
+}   
