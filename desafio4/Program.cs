54e7a023a200775f73fd979879cb48a9dfa5eefb
Victor Fernandes <joaovictorf01@gmail.com>
2018-12-01 12:47:56 -0300

    Desafio 104 feito!

---
+++ desafio4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Int32.Parse(Console.ReadLine());
+            int n2 = Int32.Parse(Console.ReadLine());
+            int PROD = n1 * n2;
+            Console.Write("PROD = "+PROD);
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ desafio4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("desafio4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("desafio4")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("03a904d5-982d-4d42-9888-8a4474aa135e")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
